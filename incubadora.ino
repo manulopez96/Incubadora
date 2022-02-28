c172750a2e3568a2444ab73a5fab541fbe8d9920
manu.lopez <d.manu.lopez@gmail.com>
2022-02-28 23:31:22 +0100

    primer commit

---
+++ incubadora.ino
@@ -0,0 +1,179 @@
+#include <TinyDHT.h>
+#include <Servo.h>
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+
+//LCD
+LiquidCrystal_I2C lcd (0x27, 2, 1, 0, 4, 5, 6, 7);
+
+
+//salidas rele
+int ventilador1 = 5;
+int ventilador2 = 6;
+int luz = 7;
+
+
+//Entradas digitales
+int pulsador = 3;
+
+
+//Sensor DHT
+int pin_dht = 2;
+int dht_humedad;
+int dht_temperatura;
+DHT dht(pin_dht,DHT11);
+
+
+//Seteo de humedad y temperatura
+int pin_humedad = A0;
+int pin_temperatura = A1;
+int set_humedad;
+int set_temperatura;
+int valor_humedad;
+int valor_temperatura;
+
+
+//control base
+Servo servo1;
+int PINSERVO = 12;
+int PULSOMIN = 600;
+int PULSOMAX = 2550;
+
+
+//control con tiempo
+unsigned long time_work = (millis()/3600000); //pasado a hora tiempo para trabajar oscilacion
+int sumador_horas = 0;
+
+
+void setup() {
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH);
+  lcd.begin(16, 2);
+  lcd.clear();
+  
+  servo1.attach(PINSERVO, PULSOMIN, PULSOMAX);
+
+  dht.begin();
+  
+  pinMode(ventilador1, OUTPUT);
+  pinMode(ventilador2, OUTPUT);
+  pinMode(luz, OUTPUT);
+  pinMode(pulsador, INPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  unsigned long time_run = (millis()/3600000) + sumador_horas; //pasado a hora, tiempo de funcionamiento
+  
+  // seteo de salidas a LOW
+  digitalWrite(ventilador1, 1);
+  digitalWrite(ventilador2, 1);
+  digitalWrite(luz, 1);
+
+  while(digitalRead(pulsador)==1){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Sumar Horas");
+    sumador_horas += 1;
+    lcd.setCursor(0, 1);
+    lcd.print(  time_run + sumador_horas);    
+    delay(500);
+  }
+
+  //seteo de humedad y temperatura
+  valor_humedad = analogRead(pin_humedad);
+  valor_temperatura = analogRead(pin_temperatura);
+  set_humedad = map(valor_humedad, 0, 1023, 20, 80);
+  set_temperatura = map(valor_temperatura, 0, 1023, 0, 50);
+
+
+  //medicion de humedad y temperatura
+  dht_humedad = dht.readHumidity();
+  dht_temperatura = dht.readTemperature();
+
+
+  // Imprimir estado de humedad y temperatura en LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(dht_temperatura);
+  lcd.print("C > ");
+  lcd.print(set_temperatura);
+  lcd.print("C | TIME");
+  lcd.setCursor(0, 1);
+  lcd.print(dht_humedad);
+  lcd.print("% > ");
+  lcd.print(set_humedad);
+  lcd.print("% | ");
+  lcd.print(time_run);
+  lcd.print("h");
+  
+  test_humedad();
+
+  
+  // control movimiento de base
+  if ((time_work <= time_run) && (time_work < 337)){ // condicion para oscilar la base
+
+    //imprimiendo estado
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("  Oscilando  ");
+
+    //moviendo base
+//    mover_base(); 
+    mover_base(); 
+    time_work = time_run + 4; // cada cuatro horas activa la oscilacion de la base 
+    imprimir_en_serial();
+    }
+}
+
+
+void mover_base (){
+
+  for(int i = 44; i<88 ; i++){
+    servo1.write(i);
+    delay(50);
+  }
+    for(int i = 88; i>44 ; i--){
+    servo1.write(i);
+    delay(50);
+  }
+  for(int i = 44; i>22 ; i--){
+    servo1.write(i);
+    delay(50);
+  }
+    for(int i = 22; i<44 ; i++){
+    servo1.write(i);
+    delay(50);
+  }
+  servo1.write(44);
+}
+
+  
+  void imprimir_en_serial(){
+    //time_work = (millis()/1000); //pasado a segundo
+    Serial.print("Tiempo trabajando: ");
+    Serial.print((millis()/3600000));
+    Serial.print("\n");  
+    Serial.print("Hora del proximo ciclo: ");
+    Serial.print(time_work);
+    Serial.print("\n");
+}
+
+void test_humedad(){
+  if(((millis()/1000)%20) == 0){ // solo imprime cada 20 segundos, para testear preset en serial
+    Serial.print("set humedad: ");
+    Serial.print(set_humedad);
+    Serial.print("\n");
+    Serial.print("set temperatura: ");
+    Serial.print(set_temperatura);
+    Serial.print("\n");
+    Serial.print("temepratura medicion: ");
+    Serial.print(dht_temperatura);
+    Serial.print(" Humedad medicion:");
+    Serial.print(dht_humedad);
+    }
+}
