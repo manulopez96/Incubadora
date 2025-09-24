59f7e106fae5d0069ce222617f5cb6c2cbdde28b
López Emanuel <65171272+manulopez96@users.noreply.github.com>
2025-09-24 09:52:23 -0300

    Update incubadora.ino

--- incubadora/incubadora.ino
+++ incubadora/incubadora.ino
@@ -4,222 +4,190 @@
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
+// ================= LCD =================
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7);
 
-//LCD
-LiquidCrystal_I2C lcd (0x27, 2, 1, 0, 4, 5, 6, 7);
+// ================= Pines =================
+const int PIN_VENT1 = 5;
+const int PIN_VENT2 = 6;
+const int PIN_LUZ   = 8;
+const int PIN_PULS  = 3;
 
+const int PIN_DHT   = 2;
+const int PIN_HUMEDAD = A0;
+const int PIN_TEMPSET = A1;
 
-//salidas rele
-int ventilador1 = 5;
-int ventilador2 = 6;
-int luz = 8;
+const int PIN_SERVO = 11;
+const int PULSOMIN  = 900;
+const int PULSOMAX  = 2100;
 
-
-//Entradas digitales
-int pulsador = 3;
-
-
-//Sensor DHT
-int pin_dht = 2;
-int dht_humedad;
-int dht_temperatura;
-DHT dht(pin_dht,DHT11);
-
-
-//Seteo de humedad y temperatura
-int pin_humedad = A0;
-int pin_temperatura = A1;
-int set_humedad;
-int set_temperatura;
-int valor_humedad;
-int valor_temperatura;
-
-
-//control base
+// ================= Objetos =================
+DHT dht(PIN_DHT, DHT11);
 Servo servo1;
-int PINSERVO = 11;
-int PULSOMIN = 900;
-int PULSOMAX = 2100;
 
+// ================= Variables =================
+int set_humedad, set_temperatura;
+int dht_humedad, dht_temperatura;
 
-//control con tiempo
-unsigned long time_work = (millis()/3600000); //pasado a hora tiempo para trabajar oscilacion
+unsigned long ultimaOscilacion = 0;
+unsigned long intervaloOscilacion = 4UL * 3600000; // 4 horas en ms
 int sumador_horas = 0;
 
-
+// ================= Setup =================
 void setup() {
+  Serial.begin(9600);
+
+  // Servo
+  servo1.attach(PIN_SERVO, PULSOMIN, PULSOMAX);
   servo1.write(25);
-  
-  lcd.setBacklightPin(3,POSITIVE);
+
+  // LCD
+  lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);
   lcd.begin(16, 2);
   lcd.clear();
-  
-  servo1.attach(PINSERVO, PULSOMIN, PULSOMAX);
 
+  // DHT
   dht.begin();
-  
-  pinMode(ventilador1, OUTPUT);
-  pinMode(ventilador2, OUTPUT);
-  pinMode(luz, OUTPUT);
-  pinMode(pulsador, INPUT);
 
-  // seteo de salidas a LOW
-  digitalWrite(ventilador1, 1);
-  digitalWrite(ventilador2, 1);
-  digitalWrite(luz, 1);
-  
-  Serial.begin(9600);
+  // Salidas
+  pinMode(PIN_VENT1, OUTPUT);
+  pinMode(PIN_VENT2, OUTPUT);
+  pinMode(PIN_LUZ, OUTPUT);
+
+  // Entradas
+  pinMode(PIN_PULS, INPUT);
+
+  // Apagar salidas
+  digitalWrite(PIN_VENT1, HIGH);
+  digitalWrite(PIN_VENT2, HIGH);
+  digitalWrite(PIN_LUZ, HIGH);
 }
 
+// ================= Loop =================
 void loop() {
+  unsigned long ahora = millis();
+  unsigned long horasFuncionando = (ahora / 3600000) + sumador_horas;
 
-  unsigned long time_run = (millis()/3600000) + sumador_horas; //pasado a hora, tiempo de funcionamiento
-  int aux = 0;
-  
-  
-  //seteo de humedad y temperatura
-  valor_humedad = analogRead(pin_humedad);
-  valor_temperatura = analogRead(pin_temperatura);
-  set_humedad = map(valor_humedad, 0, 1023, 20, 80);
-  set_temperatura = map(valor_temperatura, 0, 1023, 0, 50);
+  // Seteo de consigna desde potenciómetros
+  set_humedad = map(analogRead(PIN_HUMEDAD), 0, 1023, 20, 80);
+  set_temperatura = map(analogRead(PIN_TEMPSET), 0, 1023, 0, 50);
 
-
-  //medicion de humedad y temperatura
+  // Lectura DHT
   dht_humedad = dht.readHumidity();
   dht_temperatura = dht.readTemperature();
 
-  
-  // control temperatura y humedad
-  
-  if(dht_humedad <= set_humedad){
-    digitalWrite(ventilador1, 1);//apagado
-    digitalWrite(ventilador2, 1);//apagado
-  }  
-
-  if(dht_temperatura < set_temperatura){
-    digitalWrite(luz, 0);//prendida
+  if (!isnan(dht_humedad) && !isnan(dht_temperatura)) {
+    controlHumedad();
+    controlTemperatura();
+    mostrarLCD(horasFuncionando);
   }
 
-  if(dht_temperatura > set_temperatura * 1.1){
-    digitalWrite(ventilador1, 0);//prendido
+  controlOscilacion(ahora, horasFuncionando);
+  leerPulsador(horasFuncionando);
+  test_humedad();
+}
+
+// ================= Funciones =================
+void controlHumedad() {
+  if (dht_humedad <= set_humedad) {
+    digitalWrite(PIN_VENT1, HIGH); // apagado
+    digitalWrite(PIN_VENT2, HIGH);
+  } else {
+    digitalWrite(PIN_VENT2, LOW);  // prendido
   }
+}
 
-    if(dht_temperatura > set_temperatura * 1.05){
-    digitalWrite(luz, 1);//apagada
+void controlTemperatura() {
+  if (dht_temperatura < set_temperatura) {
+    digitalWrite(PIN_LUZ, LOW);  // prendida
+  } else if (dht_temperatura > set_temperatura * 1.1) {
+    digitalWrite(PIN_VENT1, LOW); // ventilador prendido
+  } else if (dht_temperatura > set_temperatura * 1.05) {
+    digitalWrite(PIN_LUZ, HIGH);  // apagada
   }
+}
 
-  if(dht_humedad > set_humedad){
-    digitalWrite(ventilador2, 0);//prendido
-  }
-
-
-if((millis()%10000)<200){
-    lcd.begin(16, 2); 
-  }
-
-  
-  // Imprimir estado de humedad y temperatura en LCD
+void mostrarLCD(unsigned long horas) {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(dht_temperatura);
   lcd.print("C > ");
   lcd.print(set_temperatura);
-  lcd.print("C | TIME");
+  lcd.print("C | ");
+  lcd.print(horas);
+  lcd.print("h");
+
   lcd.setCursor(0, 1);
   lcd.print(dht_humedad);
   lcd.print("% > ");
   lcd.print(set_humedad);
-  lcd.print("% | ");
-  lcd.print(time_run);
-  lcd.print("h");
-  
-  test_humedad();
+  lcd.print("%");
+}
 
-  
-  // control movimiento de base
-  if ((time_work <= time_run) && (time_work < 337)){ // condicion para oscilar la base
-
-    //imprimiendo estado
+void controlOscilacion(unsigned long ahora, unsigned long horas) {
+  if (ahora - ultimaOscilacion >= intervaloOscilacion && horas < 337) {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("  Oscilando  ");
+    lcd.print("Oscilando...");
 
-    //prende luz
-    digitalWrite(luz, 0);//prendida   
+    digitalWrite(PIN_LUZ, LOW); // prende luz
+    mover_base();
 
-    //moviendo base
-    mover_base(); 
-    mover_base(); 
-    lcd.begin(16, 2);
-    time_work = time_run + 4; // cada cuatro horas activa la oscilacion de la base 
-    imprimir_en_serial();
-    }
+    ultimaOscilacion = ahora;
+    imprimir_en_serial(horas);
+  }
+}
 
-  // sumador de horas 
-  aux = 0;
-  if(digitalRead(pulsador)==1){ 
-    while(digitalRead(pulsador)==1){
+void leerPulsador(unsigned long horas) {
+  int aux = 0;
+  if (digitalRead(PIN_PULS) == HIGH) {
+    while (digitalRead(PIN_PULS) == HIGH) {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Sumar Horas");
       aux += 1;
       lcd.setCursor(0, 1);
-      lcd.print(" ");
-      lcd.print(  time_run);    
-      lcd.print("h  >  ");
-      lcd.print(  time_run + aux);    
-      lcd.print("h  ");
+      lcd.print(horas);
+      lcd.print("h > ");
+      lcd.print(horas + aux);
+      lcd.print("h");
       delay(500);
-    }sumador_horas += aux;
-  lcd.begin(16, 2);  
-  }
-}
-
-
-void mover_base (){
-
-  for(int i = 25; i<50 ; i++){
-    servo1.write(i);
-    delay(100);
-  }
-    for(int i = 50; i>25 ; i--){
-    servo1.write(i);
-    delay(100);
-  }
-  for(int i = 25; i>0 ; i--){
-    servo1.write(i);
-    delay(100);
-  }
-  for(int i = 0; i<25 ; i++){
-    servo1.write(i);
-    delay(100);
-  }
-  servo1.write(25);
-}
-
-  
-  void imprimir_en_serial(){
-    //time_work = (millis()/1000); //pasado a segundo
-    Serial.print("Tiempo trabajando: ");
-    Serial.print((millis()/3600000));
-    Serial.print("\n");  
-    Serial.print("Hora del proximo ciclo: ");
-    Serial.print(time_work);
-    Serial.print("\n");
-}
-
-void test_humedad(){
-  if(((millis()/1000)%20) == 0){ // solo imprime cada 20 segundos, para testear preset en serial
-    Serial.print("set humedad: ");
-    Serial.print(set_humedad);
-    Serial.print("\n");
-    Serial.print("set temperatura: ");
-    Serial.print(set_temperatura);
-    Serial.print("\n");
-    Serial.print("temepratura medicion: ");
-    Serial.print(dht_temperatura);
-    Serial.print(" Humedad medicion:");
-    Serial.print(dht_humedad);
     }
+    sumador_horas += aux;
+  }
+}
+
+// Movimiento no bloqueante
+void mover_base() {
+  static int pos = 25;
+  static int direccion = 1;
+  static unsigned long ultimoMovimiento = 0;
+
+  if (millis() - ultimoMovimiento >= 100) {
+    pos += direccion;
+    if (pos >= 50 || pos <= 0) direccion *= -1;
+    servo1.write(pos);
+    ultimoMovimiento = millis();
+  }
+}
+
+void imprimir_en_serial(unsigned long horas) {
+  Serial.print("Tiempo trabajando: ");
+  Serial.println(horas);
+  Serial.print("Hora del proximo ciclo: ");
+  Serial.println((ultimaOscilacion + intervaloOscilacion) / 3600000);
+}
+
+void test_humedad() {
+  if (((millis() / 1000) % 20) == 0) {
+    Serial.print("Set humedad: ");
+    Serial.println(set_humedad);
+    Serial.print("Set temperatura: ");
+    Serial.println(set_temperatura);
+    Serial.print("Medicion T: ");
+    Serial.print(dht_temperatura);
+    Serial.print("C | H: ");
+    Serial.println(dht_humedad);
+  }
 }
